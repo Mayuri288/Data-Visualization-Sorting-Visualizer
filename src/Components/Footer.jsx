4ebcb4f0f0300ff42781d824b5fd9117eb1bccd8
Mayuri288 <82500768+Mayuri288@users.noreply.github.com>
2023-05-02 11:59:52 -0400

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+
+export default function Footer() {
+    return (
+      <footer className="text-lg-start mt-5" style={{ backgroundColor: '#69a3b2' }}>
+        <div className="container-fluid p-3">
+          <div className="row align-items-center">
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <p className="text-light mt-3 ms-3"> <b>Group #08</b></p>
+            </div>
+
+
+          </div>
+        </div>
+      </footer>
+    );
+}
